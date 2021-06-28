d4e99530431f564d60fac553b841c10ca6052212
Julia Howes <juliahowes@Julias-MacBook-Air.local>
2021-06-28 07:59:08 -0700

    about animation

--- script.js
+++ script.js
@@ -237,3 +237,37 @@ pet can connect with prospective adopters.`,
 ];
 
 fillProjects();
+
+function isElementInViewport(elem) {
+  var $elem = $(elem);
+
+  // Get the scroll position of the page.
+  var viewportTop = $('html').scrollTop();
+  var viewportBottom = viewportTop + $(window).height();
+
+  // Get the position of the element on the page.
+  var elemTop = Math.round( $elem.offset().top );
+  var elemBottom = elemTop + $elem.height();
+
+  return ((elemTop < viewportBottom) && (elemBottom > viewportTop));
+}
+
+// Check if it's time to start the animation.
+function checkAnimation() {
+  var $elem = $('.about-column');
+  var $elem2 = $('.profile-card');
+
+  // If the animation has already been started
+  if ($elem.hasClass('start')) return;
+
+  if (isElementInViewport($elem)) {
+      // Start the animation
+      $elem.addClass('start');
+      $elem2.addClass('start');
+  }
+}
+
+// Capture scroll events
+$(window).scroll(function(){
+  checkAnimation();
+});
